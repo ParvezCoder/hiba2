61ff0a238807b0fb4e845482af229f42d5db6053
hiba khaliq <hibakhaliq10@gmail.com>
2024-11-12 00:32:39 +0500

    error fixes

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import Hero from "./components/hero"
+import Contact from "./components/Contact";
+import Project from "./components/Project";
+import About from "./components/About";
+import Skill from "./components/Skill"
+export default function Home() {
+  return (
+    <div>
+      <Hero />
+      <About />
+      <Project />
+      <Contact/>
+      <Skill/>
+      
+    </div>
+  )
+}
+    
